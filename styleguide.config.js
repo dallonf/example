75fd27901b5b07da16c964fcbc818f9d9ad3f86a
Dallon Feldner <dfeldner@phunware.com>
2018-02-08 09:46:34 -0600

    Added customized Pathline

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,7 +1,12 @@
+const path = require('path');
+
 module.exports = {
 	title: 'React Style Guide Example',
 	defaultExample: true,
 	components: './lib/components/**/[A-Z]*.js',
+	styleguideComponents: {
+    Pathline: path.join(__dirname, 'lib/__styleguide__/Pathline/Pathline'),
+  },
 	webpackConfig: {
 		module: {
 			loaders: [
